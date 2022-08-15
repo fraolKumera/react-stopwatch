59312c0b46b957a0a9af562c5b9fac937921af7e
fraol <fraolkumera@gmail.com>
2022-08-15 12:29:37 +0300

    repo change

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StopWatch from './Components/StopWatch/StopWatch.jsx';
+
+function App() {
+return (
+	
+	<div className="App">
+	<StopWatch />
+	</div>
+);
+}
+
+export default App;
